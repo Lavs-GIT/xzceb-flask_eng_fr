ebe6baa6880a0bd1b75be9105e96ca804425edfa
Lavanya <lavanya.rajalingam@gmail.com>
2022-07-04 07:52:57 +0000

    updating files and project structure

--- final_project/machinetranslation/test/tests.py
+++ final_project/machinetranslation/test/tests.py
@@ -6,10 +6,12 @@ class TestEnglish_To_French(unittest.TestCase):
     def test1(self):
         self.assertEqual(english_to_french(""), "null") # test input is null
         self.assertEqual(english_to_french("Hello"), "Bonjour") # test hello translates to bonjour
+        self.assertNotEqual(english_to_french("Good"), "Mal") # test good doesnot translate to mal
 
 class TestFrench_To_English(unittest.TestCase):
     def test1(self):
         self.assertEqual(french_to_english(""), "null") # test input is null
-        self.assertEqual(french_to_english("Bonjour"), "Hello") # test hello translates to bonjour
+        self.assertEqual(french_to_english("Bonjour"), "Hello") # test bonjor translates to hello
+        self.assertNotEqual(english_to_french("Mal"), "Good") # test mal  doesnot translate to good
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,24 +1,22 @@
-import json
+"""
+This is the translator file
+"""
+
+#import json
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
-from dotenv import load_dotenv
-
 load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
-authenticator = IAMAuthenticator('{apikey}')
-language_translator = LanguageTranslatorV3(
-    version='{version}',
-    authenticator=authenticator
-)
-
-language_translator.set_service_url('https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/be85874b-7169-46e2-810e-ae031d4176cb')
+authenticator = IAMAuthenticator(apikey)
+version_lt='2018-05-01'
+language_translator = LanguageTranslatorV3(version=version_lt,authenticator=authenticator)
+language_translator.set_service_url(url)
 
 # Function for translating English to French
 
@@ -26,9 +24,8 @@ def english_to_french(text1):
     """
     This function translates english to french
     """
-    if text=="":
+    if text1=="":
         return "null"
-    
     frenchtranslation = language_translator.translate(text=text1, model_id='en-fr').get_result()
     return frenchtranslation.get("translations")[0].get("translation")
 
@@ -36,9 +33,8 @@ def french_to_english(text1):
     """
     This function translates french to english
     """
-    if text=="":
+    if text1=="":
         return "null"
-
     englishtranslation = language_translator.translate(text=text1, model_id='fr-en').get_result()
     return englishtranslation.get("translations")[0].get("translation")
-    
+
